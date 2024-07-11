29acdf614fe395154005feb669271c2cb67d3863
zhhong <zhhong@estee.com>
2024-07-11 15:09:20 +0800

    项目初始化

---
+++ src/main/java/com/ycss/asset/ctl/AssetCtlApplication.java
@@ -0,0 +1,13 @@
+package com.ycss.asset.ctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetCtlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetCtlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ycss/asset/ctl/AssetCtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycss.asset.ctl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetCtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
